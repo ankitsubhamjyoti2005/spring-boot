6b6d4d0711b004e0c8306c6d0b7ca11747f5a223
ankitsubhamjyoti2005 <ankitsubhamjyoti@gmail.com>
2024-04-16 20:55:04 +0530

    new file:   IRCTC/app/bin/main/ticket/booking/App.class 	new file:   IRCTC/app/bin/main/ticket/booking/entities/Ticket.class 	new file:   IRCTC/app/bin/main/ticket/booking/entities/Train.class 	new file:   IRCTC/app/bin/main/ticket/booking/entities/User.class 	new file:   IRCTC/app/bin/main/ticket/booking/localDB/trains.json 	new file:   IRCTC/app/bin/main/ticket/booking/localDB/users.json 	new file:   IRCTC/app/bin/main/ticket/booking/services/TrainService.class 	new file:   IRCTC/app/bin/main/ticket/booking/services/UserBookingService.class 	new file:   IRCTC/app/bin/main/ticket/booking/util/UserServiceUtil.class 	new file:   IRCTC/app/bin/test/org/example/AppTest.class 	modified:   IRCTC/app/src/main/java/ticket/booking/entities/Ticket.java 	modified:   webserver/singleThread/client.java 	modified:   webserver/singleThread/server.java

--- webserver/singleThread/client.java
+++ webserver/singleThread/client.java
@@ -1,32 +1,32 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-// import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.Socket;
-// import java.nio.Buffer;
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.io.PrintWriter;
+// // import java.net.Inet4Address;
+// import java.net.InetAddress;
+// import java.net.Socket;
+// // import java.nio.Buffer;
 
-public class client {
-    public static void main(String[] args) {
-        int port = 1800;
-        try {
-            InetAddress address = InetAddress.getByName("localhost");
-            Socket socket = new Socket(address,port);
-            PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
-            BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            toSocket.println("hello from the client");
-            String line = fromSocket.readLine();
-            System.out.println("response from the socket is: "+ line);
+// public class client {
+//     public static void main(String[] args) {
+//         int port = 1800;
+//         try {
+//             InetAddress address = InetAddress.getByName("localhost");
+//             Socket socket = new Socket(address,port);
+//             PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
+//             BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//             toSocket.println("hello from the client");
+//             String line = fromSocket.readLine();
+//             System.out.println("response from the socket is: "+ line);
 
-            toSocket.close();
-            fromSocket.close();
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//             toSocket.close();
+//             fromSocket.close();
+//             socket.close();
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
 
 
--- webserver/singleThread/server.java
+++ webserver/singleThread/server.java
@@ -1,35 +1,35 @@
-import java.io.*;
-import java.net.*;
+// import java.io.*;
+// import java.net.*;
 
-public class server{
-    public void run(){
-        int port = 1800;
-        try{
-            ServerSocket socket = new ServerSocket(port);
-            socket.setSoTimeout(1000000);
-            while (true) {
-                try{
-                    System.out.println("server is lishen on port"+port);
-                    Socket acceptedConnection = socket.accept();
-                    System.out.println("connection accept from the client"+acceptedConnection.getRemoteSocketAddress());
-                    PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
-                    BufferedReader fromClent = new BufferedReader(new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream())));
-                    toClient.println("hello from server");
-                }catch(IOException ex){
-                    ex.printStackTrace();
-                }
-            }
-        }catch(Exception c){
-            System.out.println(c);
-        }
-    }
-    public static void main(String[] args) {
-        server server = new server();
-        try{
-            server.run();
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
-    }
-}
+// public class server{
+//     public void run(){
+//         int port = 1800;
+//         try{
+//             ServerSocket socket = new ServerSocket(port);
+//             socket.setSoTimeout(1000000);
+//             while (true) {
+//                 try{
+//                     System.out.println("server is lishen on port"+port);
+//                     Socket acceptedConnection = socket.accept();
+//                     System.out.println("connection accept from the client"+acceptedConnection.getRemoteSocketAddress());
+//                     PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
+//                     BufferedReader fromClent = new BufferedReader(new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream())));
+//                     toClient.println("hello from server");
+//                 }catch(IOException ex){
+//                     ex.printStackTrace();
+//                 }
+//             }
+//         }catch(Exception c){
+//             System.out.println(c);
+//         }
+//     }
+//     public static void main(String[] args) {
+//         server server = new server();
+//         try{
+//             server.run();
+//         }catch(Exception ex){
+//             ex.printStackTrace();
+//         }
+//     }
+// }
 
