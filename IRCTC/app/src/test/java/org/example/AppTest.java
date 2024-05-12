18dca545391966a2426b1667349038a512ca40c5
ankitsubhamjyoti2005 <ankitsubhamjyoti@gmail.com>
2024-05-12 11:39:56 +0530

    modified:   IRCTC/app/src/test/java/org/example/AppTest.java 	modified:   spring-project-1/.idea/compiler.xml 	modified:   spring-project-1/.idea/gradle.xml 	modified:   spring-project-1/.idea/misc.xml 	modified:   spring-project-1/app/build.gradle 	modified:   spring-project-1/app/src/main/java/org/example/App.java 	deleted:    spring-project-1/untitled/.idea/.gitignore 	deleted:    spring-project-1/untitled/.idea/misc.xml 	deleted:    spring-project-1/untitled/.idea/modules.xml 	deleted:    spring-project-1/untitled/.idea/vcs.xml 	deleted:    spring-project-1/untitled/untitled.iml

--- IRCTC/app/src/test/java/org/example/AppTest.java
+++ IRCTC/app/src/test/java/org/example/AppTest.java
@@ -9,8 +9,5 @@ import ticket.booking.App;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+
 }
